0ef8be8f7d44b4c1334d120e60f53de0b19fdaee
Persio Godoy <persio.godoy@novasingular.com.br>
2025-10-26 13:37:39 -0300

    feat: Atualizar menu de navegação para corrigir a ordem dos links de clientes e sprints

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,9 +6,9 @@ export function Menu() {
   return (
     <nav className="bg-purple-600 text-white p-4">
       <div className="max-w-4xl mx-auto flex space-x-4">
-        <Link href="/clients">Clientes</Link>
         <Link href="/dashboard">Dashboard</Link>
-        <Link href="/sprint">Criar Sprint</Link>
+        <Link href="/clients">Clientes</Link>
+        <Link href="/sprint">Sprints</Link>
         <Link href="/tasks">Atividades</Link>
       </div>
     </nav>
